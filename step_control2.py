b3d423c82088f31a6523dd928f8d7fb2c1508f9c
Can Karaçelebi <37347701+cankaracelebi@users.noreply.github.com>
2022-12-19 22:16:29 +0300

    py file

---
+++ step_control2.py
@@ -0,0 +1,37 @@
+from machine import Pin
+
+step_one = Pin(2,Pin.OUT)
+direction_one = Pin(3,Pin.OUT)
+step_two = Pin(8,Pin.OUT)
+direction_two = Pin(9, Pin.OUT)
+spr = 4000
+
+while True:
+    direction_one.high()
+    direction_two.high()
+    for i in range(spr):
+        step_one.high()
+        step_two.high()
+
+        utime.sleep(0.001)
+
+        step_one.low()
+        step_two.low()
+
+        utime.sleep(0.001)
+    utime.sleep(2)
+    direction_one.low()
+    direction_two.low()
+
+    for j in range(spr):
+        step_one.high()
+        step_two.high()
+        utime.sleep(0.001)
+
+        step_one.low()
+        step_two.low()
+
+        utime.sleep(0.001)
+    utime.sleep(2)
+
+
